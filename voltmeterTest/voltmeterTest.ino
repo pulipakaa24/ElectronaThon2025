572994e8e5872edb7a4ed85381221e540936debb
Aditya Pulipaka <adipu@Mac.attlocal.net>
2025-03-16 22:48:26 -0500

    Cleaned up code and added comments

--- voltmeterTest/voltmeterTest.ino
+++ voltmeterTest/voltmeterTest.ino
@@ -8,9 +8,8 @@
 
 #define zoomOut 12
 #define zoomIn 7
+#define squareWaveTest 3
 
-// #define TFT_SCLK 13   // set these to be whatever pins you like!
-// #define TFT_MOSI 11   // set these to be whatever pins you like!
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 int zoomScale = 4;
@@ -18,75 +17,48 @@ int zoomScale = 4;
 void setup() {
   // Initialize serial communication
   Serial.begin(9600);
-
-  // Initialize the TFT screen with 128x160 resolution
-  tft.initR(INITR_BLACKTAB);  // Use this initialization method
-
+  tft.initR(INITR_BLACKTAB);
   Serial.println("Initialized");
+
   // Fill the screen with black color
   tft.fillScreen(ST7735_BLACK);
 
-  // Optionally, display some text for testing
-  tft.setTextColor(ST7735_WHITE); 
-  tft.setTextSize(1); 
-  tft.setCursor(10, 10); 
-  pinMode(3, OUTPUT);
+  pinMode(squareWaveTest, OUTPUT);
   pinMode(zoomOut, INPUT);
   pinMode(zoomIn, INPUT);
 }
 
-int voltReads[160];
-int output = 0;
-bool high = false;
+int voltReads[160]; // array of voltage readings to populate screen with
+// maximum size of array is width of screen.
 
 bool outbuttonPress = false;
 bool inbuttonPress = false;
 
 void loop() {
   int i;
-  // put your main code here, to run repeatedly:
-  // Serial.print("Voltage read: ");
+
+  // Half duty square wave
   analogWrite(3, 122);
+
+  // Take as many measurements as determined by the zoomscale - only so many points can fit on the screen!
   for(i = 0; i < 160/zoomScale; i++) {
-    // if (high) {
-    //   if (output>0) {
-    //     analogWrite(3, output);
-    //     output--;
-    //   }
-    //   else {
-    //     high = false;
-    //   }
-    // }
-    // else {
-    //   if (output < 255) {
-    //     analogWrite(3, output);
-    //     output++;
-    //   }
-    //   else {
-    //     high = true;
-    //   }
-    // }
+    // populate array with mappings from ADC scale to width of LCD panel, with 0V in the middle.
     voltReads[i] = map(analogRead(A0)  - analogRead(A5), -1023, 1023, 0, 127);
   }
 
+  // Mark the pixels for each data point and connect them with lines
   for(i = 0; i < 160/zoomScale; i++) {
-    tft.fillCircle(voltReads[i], i*zoomScale, 0, ST77XX_WHITE);
+    tft.fillCircle(voltReads[i], i*zoomScale, 0, ST7735_WHITE);
     if (i * zoomScale < 159) {
       tft.drawLine(voltReads[i], i * zoomScale, voltReads[i+1], (i+1) * zoomScale, ST7735_WHITE);
     }
   }
-  // digitalWrite(bit0, HIGH);
-  delay(100);
-  tft.fillScreen(0);
-  // for(i = 0; i < 160; i++) {
-  //   tft.fillCircle(voltReads[i], i, 0, ST77XX_BLACK);
-  //   if (i < 159) {
-  //     if (voltReads[i] != voltReads[i+1]) {
-  //       tft.drawLine(voltReads[i], i, voltReads[i+1], i+1, ST7735_BLACK);
-  //     }
-  //   }
-  // }
 
+  delay(100);
+  // 100 Millis after finished populating, clear screen.
+  tft.fillScreen(0);
+
+  // Zooming functionality with debouncing.
   if (digitalRead(zoomOut) && !outbuttonPress) {
     if (zoomScale>1) {
       zoomScale--;
