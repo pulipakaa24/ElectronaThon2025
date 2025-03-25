6766d737fae2d7b432754c3c67a3699d666a4cec
Aditya Pulipaka <adipu@wireless-10-154-238-109.public.utexas.edu>
2025-03-25 12:51:59 -0500

    Implemented Gautham's python code with sine and cosine in arduino - pending testing

--- FFTTest/FFTTest.ino
+++ FFTTest/FFTTest.ino
@@ -7,19 +7,24 @@
 #define SAMPLES_IN 32            // Number of samples for FFT
 #define SAMPLES_OUT 128
 #define SAMPLING_FREQUENCY 1000  // Sampling frequency in Hz (1 kHz)
-#define signalPin 5
-
+#define SQUAREWAVE_FREQ 100 // frequency of square wave output for testing
 #define MAXVAL 1023
+
 // Define the pins connected to your ST7735R display
 #define TFT_CS    10  // Chip select pin (CS)
 #define TFT_DC    9   // Data/Command pin (DC)
 #define TFT_RST   8   // Reset pin (RST)
+#define signalPin 5
+#define posTerminal A0
+#define negTerminal A5
 
 // Array to hold the real and imaginary parts of the FFT
 double uReal[SAMPLES_IN];
 double uImag[SAMPLES_IN];
-double vReal[SAMPLES_OUT];
-double vImag[SAMPLES_OUT];
+double outResults[SAMPLES_OUT];
+double timeSpacing[SAMPLES_OUT];
+
+double t_TOT = (double)SAMPLES_IN/(double)SAMPLING_FREQUENCY;
 
 // Create FFT object
 ArduinoFFT<double> FFT = ArduinoFFT<double>(uReal, uImag, SAMPLES_IN, SAMPLING_FREQUENCY);
@@ -40,38 +45,36 @@ void setup() {
   
   tft.initR(INITR_BLACKTAB);
   tft.fillScreen(ST7735_BLACK);
-  // pinMode(signalPin, OUTPUT);
+  pinMode(signalPin, OUTPUT);
 
-  // Timer1.initialize(8000);
-  // Timer1.attachInterrupt(toggleSignal);
+  for(int i = 0; i < SAMPLES_OUT; i++) {
+    timeSpacing[i] = t_TOT*(double)i/SAMPLES_OUT;
+  }
+
+  Timer1.initialize(1000000/(SQUAREWAVE_FREQ*2));
+  Timer1.attachInterrupt(toggleSignal);
 }
 
-void realResult(double a[], double b[], int N, double tToRes[], int samplesOut) {
+void realResult(double a[], double b[], int N, double t[], int samplesOut, double res[]) {
   for (int i = 0; i < samplesOut; i++) {
     double sum = 0.0;
     for (int n = 0; n < (N+1)/2 + 1; n++) {
-      sum += a[n]*cos(2*M_PI*(double)n*tToRes[i]) - b[n]*sin(2*M_PI*(double)n*tToRes[i]);
+      sum += a[n]*cos(2*M_PI*(double)n*t[i]) - b[n]*sin(2*M_PI*(double)n*t[i]);
     }
     for (int n = (N+1)/2 + 1; n < N; n++) {
-      sum += a[n]*cos(2*M_PI*(double)(n - N)*tToRes[i]) - b[n]*sin(2*M_PI*(double)(n - N)*tToRes[i]);
+      sum += a[n]*cos(2*M_PI*(double)(n - N)*t[i]) - b[n]*sin(2*M_PI*(double)(n - N)*t[i]);
     }
-    tToRes[i] = sum;
+    res[i] = sum;
   }
 }
 
 void loop() {
   for (int i = 0; i < SAMPLES_IN; i++) {
-    uReal[i] = analogRead(A5);
-  }
-  // Nothing to do here, as we're just doing a one-time FFT on the samples
-  // Sampling the signal at a fixed rate
-  // FFT.setArrays(uReal, uImag, SAMPLES_IN);
-  
-  for (int i = 0; i < SAMPLES_IN; i++) {
-    uReal[i] = 1023*((i/10)%2);  // Read analog input
-    uImag[i] = 0;  // Imaginary part is 0 for real signal
+    uReal[i] = analogRead(posTerminal) - analogRead(negTerminal);
     delayMicroseconds(1000);  // Wait for the next sample (based on SAMPLING_FREQUENCY)
   }
+
+  // FFT.setArrays(uReal, uImag, SAMPLES_IN);
 
   // Perform FFT
   FFT.windowing(FFTWindow::Hamming, FFTDirection::Forward);  // Apply Hamming window
@@ -82,17 +85,12 @@ void loop() {
   // FFT.setArrays(vReal, vImag, SAMPLES_OUT);
   // FFT.compute(FFTDirection::Reverse); // Inverse FFT
 
-  // for (int i = 0; i < 32; i++) {
-  //   Serial.print(i);
-  //   Serial.print("|");
-  //   Serial.print(uReal[i]);
-  //   Serial.print("|");
-  //   Serial.println(uImag[i]);
-  // }
+  realResult(uReal, uImag, SAMPLES_IN, timeSpacing, SAMPLES_OUT, outResults);
+
   tft.fillScreen(0);
   for (int i = 0; i < 127; i++) {
-    tft.drawLine(i, map(uReal[i], -1*MAXVAL, MAXVAL, 0, 159), i+1, 
-                  map(uReal[i+1], -1*MAXVAL, MAXVAL, 0, 159), ST7735_BLUE);
+    tft.drawLine(i, constrain(map(outResults[i], -1*MAXVAL, MAXVAL, 0, 159), 0, 159), i+1, 
+                  constrain(map(outResults[i+1], -1*MAXVAL, MAXVAL, 0, 159), 0, 159), ST7735_BLUE);
   }
   delay(100);
 }
